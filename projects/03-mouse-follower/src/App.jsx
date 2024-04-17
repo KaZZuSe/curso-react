6f6688bb865501ceea4819c20d921f4daa6459fa
Use <usemartin2@gmail.com>
2024-04-17 23:51:29 +0200

    changes

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,17 +1,53 @@
 import { useState, useEffect } from 'react'
 
-
-function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: -50, y: -50 })
 
   useEffect(() => {
-    console.log('efecto')
-  })
+    console.log('efecto', { enabled })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    {/* hook nunca dentro de condicional */}
+    if (enabled) { 
+      window.addEventListener('pointermove', handleMove)
+    } else{
+      setPosition({ x: -50, y: -50 })
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+
+    }
+  }, [enabled])
   return (
     <>
-      <h3>Proyecto 3</h3>
-      <button>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
     </>
+  )
+  
+}
+function App() {
+  return (
+    <FollowMouse />
   )
 }
 
